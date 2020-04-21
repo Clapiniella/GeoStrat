85dc28270b76502f780744e7d304a16ad8f64d75
Clara <clapiniella@gmail.com>
2020-04-21 13:20:51 +0200

    20 abr

--- src/function.py
+++ src/function.py
@@ -98,6 +98,29 @@ def nearbyAPI(location, radius, keyword):
             places.extend(results['results'])
         return places 
 
+def search_places_by_input(query=''):
+    # Search for a list of places in a city using places API from google.
+    apiKey = os.getenv("API_KEY")
+    endpoint_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    places = []
+    params = {
+        'query': input("Query: "),
+        'key': apiKey
+    }
+    res = requests.get(endpoint_url, params = params)
+    results = json.loads(res.content)
+    print(results)
+    places.extend(results['results'])
+    time.sleep(2)
+    while "next_page_token" in results:
+        # Get the results from the 2nd and 3th page of the search in the API, with a total of 60 results.
+        params['pagetoken'] = results['next_page_token'],
+        res = requests.get(endpoint_url, params = params)
+        results = json.loads(res.content)
+        places.extend(results['results'])
+        time.sleep(2)
+    return places
+
 def createApiDf(query):
     name=[]
     long=[]
